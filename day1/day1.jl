f326af8892d48f6d9194b188967e85f1f174406a
aTrotier <a.trotier@gmail.com>
2024-12-02 14:38:22 +0100

    fix

--- day1/day1.jl
+++ day1/day1.jl
@@ -1,6 +1,6 @@
 using DelimitedFiles
 
-function day1(input_path::AbstractString)
+function day1_distance(input_path::AbstractString)
   input = Int.(readdlm(input_path))
   l1 = sort(input[:,1])
   l2 = sort(input[:,2])
@@ -8,5 +8,27 @@ function day1(input_path::AbstractString)
 end
 
 
-day1("../ressources/day1_test.txt")
-day1("../ressources/day1_input.txt")
+day1_distance("ressources/day1_test.txt")
+day1_distance("ressources/day1_input.txt")
+
+
+####
+function day1_similarity(input_path::AbstractString)
+  input = Int.(readdlm(input_path))
+
+  d=Dict{Int,Int}()
+  res = 0
+  for i in axes(input,1)
+    keyval = input[i,1]
+    if ~haskey(d,input[i])
+      d[keyval] = keyval*sum(input[:,2].==keyval)
+    end
+    res = res + d[keyval]
+  end
+
+  return res
+end
+
+
+day1_similarity("ressources/day1_test.txt")
+day1_similarity("ressources/day1_input.txt")
