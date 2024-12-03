234822598ab1dbe9e867c619bcbf9ab638e32a53
aTrotier <a.trotier@gmail.com>
2024-12-03 16:44:01 +0100

    day3-part2

--- day3/day3.jl
+++ day3/day3.jl
@@ -18,3 +18,27 @@ end
 
 res = sum(sum_mul.(l))
 println("results = $res")
+
+
+### Part 2 : proccess only string after do()
+path_ = joinpath(@__DIR__,"ressources/input.txt")
+l = join(readlines(path_))
+#l=join(l)
+
+function count_do(l::AbstractString)::Int
+  l2 = split.(l,"don't()")
+  l3 = split.(l2,"do()")
+
+  keep=[]
+  push!(keep,l2[1]) # first line start with do()
+  for i in axes(l3,1)[2:end]
+    for j =  2:length(l3[i])
+      push!(keep,l3[i][j])
+    end
+  end
+
+  return sum(sum_mul.(keep))
+end
+
+res = sum(count_do.(l))
+println("results = $res")
