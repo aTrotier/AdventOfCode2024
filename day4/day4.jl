2a23b748a3b452a04760b334108b325835ab5b38
aTrotier <a.trotier@gmail.com>
2024-12-04 10:40:47 +0100

    day4 part1

---
+++ day4/day4.jl
@@ -0,0 +1,34 @@
+# Horizontal / vertical / diagonal (+/- backward)
+using LinearAlgebra: diag
+
+path_ = joinpath(@__DIR__,"ressources/input.txt")
+l = readlines(path_)
+
+M = Matrix{Char}(undef,size(l,1),length(l[1]))
+for i in axes(l,1)
+  M[i,:]=[l[i]...]
+end
+M
+
+# create all comb
+vec_xmas = []
+for i in axes(M,2) # horizontal
+  push!(vec_xmas,string(M[i,:]...))
+end
+for i in axes(M,1) # vertical
+  push!(vec_xmas,string(M[:,i]...))
+end
+for i in -size(M,1)+1:1:size(M,1)-1 # diagonal
+  push!(vec_xmas,string(diag(M,i)...))
+  push!(vec_xmas,string((diag(rotl90(M),i)...)))
+end
+
+# also reverse
+vec_tot = vcat(vec_xmas,[reverse(vec_xmas[i]) for i in eachindex(vec_xmas)])
+
+re = r"XMAS"
+count_XMAS = 0
+for i in eachindex(vec_tot)
+  count_XMAS += length(collect(eachmatch(re,vec_tot[i])))
+end
+count_XMAS
